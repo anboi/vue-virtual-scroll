9daad69fed697de03e2c664dd7d656b7f1c0908a
Boyang An <boyang.an@yahoo.com>
2021-12-02 05:15:54 -0500

    webApp: formatting the code

--- src/components/HorizontalVirtualScrollTable.vue
+++ src/components/HorizontalVirtualScrollTable.vue
@@ -1,56 +1,59 @@
 <template>
     <div>
-    <div :style="styles['table-container']" v-on:scroll="onTableXScroll" id="horizontal-virtual-scroll-table-container">
-        <div :style="styles['whole-contents']">
-            <div 
-                :style="styles['current-contents']"
-            >
-        <table :style="styles['table']" v-if="privateState.currentData">
-            <thead>
-                <tr>
-                    <th 
-                        v-for="(field, index) in privateState.currentData[0]" 
-                        :key="index"
-                        :style="styles['col']"
-                    >
-                        {{field}}
-                    </th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(row, index) in privateState.currentData" :key="index">
-                    <td v-for="(col, index) in row" :key="index">
-                        {{col}}
-                    </td>
-                </tr>
-            </tbody>
-        </table>
-        
-            </div>
-        </div>
-       
-    </div>
-     <p>document.querySelectorAll('*').length: {{getAllItemsCount()}}</p>
+      <div :style="styles['table-container']" v-on:scroll="onTableXScroll" id="horizontal-virtual-scroll-table-container">
+          <div :style="styles['whole-contents']">
+              <div 
+                  :style="styles['current-contents']"
+              >
+                <table :style="styles['table']" v-if="privateState.currentData">
+                    <thead>
+                        <tr>
+                            <th 
+                                v-for="(field, index) in privateState.currentData[0]" 
+                                :key="index"
+                                :style="styles['col']"
+                            >
+                                {{field}}
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(row, index) in privateState.currentData" :key="index">
+                            <td v-for="(col, index) in row" :key="index">
+                                {{col}}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+              </div>
+          </div>
+      </div>
+      <p>document.querySelectorAll('*').length: {{getAllItemsCount()}}</p>
      </div>
 </template>
 
 <script>
-var randomstring = require("randomstring");
+/* for local testing purpose */
+// var randomstring = require("randomstring");
+
+/* npm package for converting csv to json format */
 var Papa = require("papaparse");
+
 const TABLE_HEIGHT = 50;
 const TABLE_WIDTH = 80;
 const COL_WIDTH = 200;
+
 export default {
   name: "HorizontalVirtualScrollTable",
   data() {
     return {
       privateState: {
-        rowNum: 100,
-        colNum: 1000,
-        stringLen: 10,
+        // rowNum: 100,
+        // colNum: 1000,
+        // stringLen: 10,
         tableData: null,
         currentData: null,
-        tableOverflowX: 0
+        // tableOverflowX: 0
       },
       sharedState: {},
       styles: {
@@ -67,7 +70,6 @@ export default {
         table: {
           tableLayout: "fixed",
           width: TABLE_WIDTH + "vw"
-          //   width: this.privateState.tableData[0].length * COL_WIDTH + 'px',
         },
         col: {
           width: COL_WIDTH + "px"
@@ -76,6 +78,9 @@ export default {
     };
   },
   mounted() {
+    /**
+     * Loading csv file
+     */
     var that = this;
     Papa.parse(
       "https://raw.githubusercontent.com/anboi/vue-virtual-scroll/master/src/components/acho_export_virtual_scroll.csv",
@@ -102,21 +107,22 @@ export default {
     );
   },
   computed: {
-    getTableData: function() {
-      let table = {
-        schema: [],
-        data: []
-      };
-      for (let i = 0; i < this.privateState.rowNum; i++) {
-        let row = [];
-        for (let j = 0; j < this.privateState.colNum; j++) {
-          if (i == 0) table["schema"].push("field_" + j);
-          row.push(randomstring.generate(this.privateState.stringLen));
-        }
-        table["data"].push(row);
-      }
-      return table;
-    }
+    /* for local tsting  */
+    // getTableData: function() {
+    //   let table = {
+    //     schema: [],
+    //     data: []
+    //   };
+    //   for (let i = 0; i < this.privateState.rowNum; i++) {
+    //     let row = [];
+    //     for (let j = 0; j < this.privateState.colNum; j++) {
+    //       if (i == 0) table["schema"].push("field_" + j);
+    //       row.push(randomstring.generate(this.privateState.stringLen));
+    //     }
+    //     table["data"].push(row);
+    //   }
+    //   return table;
+    // }
   },
   methods: {
     onTableXScroll() {
