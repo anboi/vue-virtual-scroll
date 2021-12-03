66021808c07e74ade9791093c334f0f983b11464
unknown <sarat.sankula@rd.loreal.com>
2021-12-03 12:21:59 -0500

    webApp: only read first 50 rows of input data

--- src/components/HorizontalVirtualScrollTable.vue
+++ src/components/HorizontalVirtualScrollTable.vue
@@ -1,59 +1,62 @@
 <template>
     <v-container>
-      <v-row :justify="'center'" :align="'center'" :style="{height: '100vh'}">
+      <v-row :justify="'center'" :align="'center'" :style="{height: '100vh',}">
         <v-col>
           <v-row :justify="'center'">
             <p :style="{fontSize: '2rem', fontWight: 'bold',}">Horizontal Virtual Scroll Table</p>
           </v-row>
-          <div :style="{height: '2rem'}" />
-          <div v-if="!privateState.currentData">
-              <v-progress-circular
-                :width="3"
-                color="green"
-                indeterminate
-              ></v-progress-circular>
-          </div>
-          <v-sheet
-            color="white"
-            elevation="2"
-            rounded
-          >
-            <v-row no-gutters>
-              <div :style="styles['table-container']" v-on:scroll="onTableXScroll" id="horizontal-virtual-scroll-table-container">
-                  <div :style="styles['whole-contents']">
-                      <div 
-                          :style="styles['current-contents']"
-                      >
-                          <table :style="styles['table']" v-if="privateState.currentData">
-                              <thead>
-                                  <tr>
-                                      <th 
-                                          v-for="(item, index) in privateState.currentData[0]" 
-                                          :key="index"
-                                          :style="getHeaderStyle(item['originalIndex'])"
-                                      >
-                                          {{item['data']}}
-                                      </th>
-                                  </tr>
-                              </thead>
-                              <tbody>
-                                  <tr v-for="(row, index) in privateState.currentData.slice(1)" :key="index" :style="styles['row']">
-                                      <td v-for="(col, index) in row" :key="index" :style="styles['col']">
-                                          <div v-if="!col" :style="{color: 'lightgrey'}">null</div>
-                                          <div v-else>{{col}}</div>
-                                      </td>
-                                  </tr>
-                              </tbody>
-                          </table>
-                      </div>
-                  </div>
-              </div>
-            </v-row>
-            <div :style="{height: '2rem'}" />
-            <v-row :justify="'center'" no-gutters>
-              <p>document.querySelectorAll('*').length: {{getAllItemsCount()}}</p>
-            </v-row>
-          </v-sheet>
+          <v-row :justify="'center'" :style="{margin: '2rem 0 2rem 0'}">
+            <div v-if="!privateState.currentData">
+                <v-progress-circular
+                  :width="3"
+                  color="green"
+                  indeterminate
+                ></v-progress-circular>
+            </div>
+          </v-row>
+          <v-row :justify="'center'">
+            <v-sheet
+              color="white"
+              elevation="2"
+              rounded
+            >
+              <v-row no-gutters>
+                <div :style="styles['table-container']" v-on:scroll="onTableXScroll" id="horizontal-virtual-scroll-table-container">
+                    <div :style="styles['whole-contents']">
+                        <div 
+                            :style="styles['current-contents']"
+                        >
+                            <table :style="styles['table']" v-if="privateState.currentData">
+                                <thead>
+                                    <tr>
+                                        <th 
+                                            v-for="(item, index) in privateState.currentData[0]" 
+                                            :key="index"
+                                            :style="getHeaderStyle(item['originalIndex'])"
+                                        >
+                                            {{item['data']}}
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="(row, index) in privateState.currentData.slice(1)" :key="index" :style="styles['row']">
+                                        <td v-for="(col, index) in row" :key="index" :style="styles['col']">
+                                            <div v-if="!col" :style="{color: 'lightgrey'}">null</div>
+                                            <div v-else>{{col}}</div>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+              </v-row>
+              <div :style="{height: '2rem'}" />
+              <v-row :justify="'center'" no-gutters>
+                <p>document.querySelectorAll('*').length: {{getAllItemsCount()}}</p>
+              </v-row>
+            </v-sheet>
+          </v-row>
         </v-col>
       </v-row>
     </v-container>
@@ -86,18 +89,17 @@ export default {
         "table-container": {
           maxHeight: TABLE_HEIGHT + "vh",
           width: TABLE_WIDTH + "vw",
-          overflow: "auto"
+          overflow: "auto",
         },
         "whole-contents": {
           width: "100%",
-          overflow: "hidden"
+          overflow: "hidden",
         },
         "current-contents": {},
         table: {
           tableLayout: "fixed",
           width: TABLE_WIDTH + "vw",
           borderCollapse: "collapse",
-          
         },
         row: {
           height: ROW_HEIGHT + "px",
@@ -134,22 +136,23 @@ export default {
               "data": item,
             };
           });
-          that.privateState.tableData = results.data;
+          let first50Rows = results.data.slice(0, 52);
+          that.privateState.tableData = first50Rows;
 
-          let colsWidth = Array(results.data[0].length).fill(100);
+          let colsWidth = Array(first50Rows[0].length).fill(100);
 
           /* calculate each column width */
-          for(let i = 0; i < results.data.length; i++){
-            for(let j = 0; j < results.data[i].length; j++){
-              if(results.data[i][j].length * COL_FONT_SIZE + COL_PADDING * 2 > colsWidth[j]){
-                  colsWidth[j] = results.data[i][j].length * COL_FONT_SIZE + COL_PADDING * 2;
+          for(let i = 0; i < first50Rows.length; i++){
+            for(let j = 0; j < first50Rows[i].length; j++){
+              if(first50Rows[i][j].length * COL_FONT_SIZE + COL_PADDING * 2 > colsWidth[j]){
+                  colsWidth[j] = first50Rows[i][j].length * COL_FONT_SIZE + COL_PADDING * 2;
               }
             }
           }
           that.privateState.colsWidth = [...colsWidth];
           
           /* calculate accumulated columns width */
-          that.privateState.accuColsWidth = Array(results.data[0].length).fill(0);
+          that.privateState.accuColsWidth = Array(first50Rows[0].length).fill(0);
           that.privateState.accuColsWidth[0] = that.privateState.colsWidth[0];
           for(let i = 1; i < results.data[0].length; i++){
             that.privateState.accuColsWidth[i] = that.privateState.accuColsWidth[i - 1] + that.privateState.colsWidth[i];
@@ -159,7 +162,7 @@ export default {
           let startIndex = that.getColStartIndex();
           let endIndex = that.getColEndIndex();
 
-          that.privateState.currentData = results.data.map(row => {
+          that.privateState.currentData = first50Rows.map(row => {
             return row.slice(startIndex, endIndex + 1);
           });
 
